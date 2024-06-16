b615b0a61d2785f0d3c2738b1e5c65858f334379
Karolina <karolina.grabowska1993@gmail.com>
2024-06-16 15:33:55 +0200

    setup project - utworzony formularz do rejestracji

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,18 @@ from django.urls import include, path
 
 import listings
 from listings import views
+from listings import forms
 from listings.views import hello
+from listings.views import signup
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls')),
-   ]
+    path('signup_form/', SignupForm.as_view(), name ='signup'),
+    path('update_form/<pk>', ConfigUpdateView.as_view(), name='update_form'),
+    path('delete/<pk>', ConfigDeleteView.as_view(), name='delete'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout')
+]
